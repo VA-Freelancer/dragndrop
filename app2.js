a89e6f720fd386a007c38f73ae72e12ef172c177
Vikulov Andrey <vikulov1@outlook.com>
2021-06-16 17:30:08 +0400

    Initial commit

---
+++ app2.js
@@ -0,0 +1,43 @@
+const items = document.querySelectorAll('.item')
+const placeholders = document.querySelectorAll('.placeholder')
+
+let itemDragging = null;
+
+for (const item of items) {
+	item.addEventListener('dragstart', dragstart)
+	item.addEventListener('dragend', dragend)
+}
+for (const placeholder of placeholders) {
+	placeholder.addEventListener('dragover', dragover)
+	placeholder.addEventListener('dragenter', dragenter)
+	placeholder.addEventListener('dragleave', dragleave)
+	placeholder.addEventListener('drop', dropdrop)
+}
+
+function dragstart(event) {
+	event.target.classList.add('hold')
+	itemDragging = event.target;
+	setTimeout(() => event.target.classList.add('hide'), 0)
+
+}
+
+function dragend(event) {
+	// event.target.classList.remove('hold', 'hide')
+	event.target.className = 'item'
+}
+
+function dragover(event) {
+	event.preventDefault()
+}
+function dragenter(event) {
+	event.target.classList.add('hovered')
+}
+function dragleave(event) {
+	event.target.classList.remove('hovered')
+
+}
+function dropdrop(event) {
+	event.target.classList.remove('hovered')
+	console.log(itemDragging)
+	event.target.append(itemDragging)
+}
